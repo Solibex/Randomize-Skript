d887efd5208b8ebb24f415176333a5109f9a98c6
Solibex <92078408+Solibex@users.noreply.github.com>
2021-11-06 19:49:10 +0700

    nothing

--- hack.js
+++ hack.js
@@ -1,7 +1,9 @@
 let retrymax = 3
 function retry(data) {
-	while retrytimes != 5 {
+	while (retrytimes != 5) {
 		retrytimes += 1
 		console.log(retrytimes)
 	}
+	console.log("Retry",data)
 }
+retry("air")
