f35350f547e403fc256b548c546fc040c825f86c
Solibex <92078408+Solibex@users.noreply.github.com>
2021-11-06 19:55:33 +0700

    hecker LMAO

--- hack.js
+++ hack.js
@@ -1,9 +1,1 @@
-let retrymax = 3
-function retry(data) {
-	while (retrytimes != 5) {
-		retrytimes += 1
-		console.log(retrytimes)
-	}
-	console.log("Retry",data)
-}
-retry("test")
+console.log("hecker")
